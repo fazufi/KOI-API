eb2e94015438e4ced2087f6c09f5c0a74d324098
fazufi <fazufi10@gmail.com>
2021-04-24 05:52:51 +0700

    post rekap

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const bodyParser = require("body-parser");
 const favicon = require("serve-favicon");
 const path = require("path");
 
-
 const db = require("./helper/knex");
 
 const app = express();
@@ -19,7 +18,6 @@ app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
-
 
 app.use("/", require("./src/routes"));
 app.listen(5000, () => console.log("Server started on port 5000"));
--- src/controllers/peserta.js
+++ src/controllers/peserta.js
@@ -1,4 +1,3 @@
-
 exports.allGet = async (req, res) => {
   try {
     const result = await req.db("peserta").orderBy("nim", "asc");
@@ -38,17 +37,32 @@ exports.post = async (req, res) => {
     const [isPeserta] = await req
       .db("peserta")
       .where({ email: req.body.email });
+      
     if (!isPeserta) {
       req.body.nim =
         (await new Date().getFullYear().toString().slice(2, 4)) +
         Math.floor(Math.random() * 5);
       req.body.created_at = await new Date();
-      req.body.updated_at = await 0
+      req.body.updated_at = await 0;
       await req.db("peserta").insert(req.body);
+
+      const peserta = await req.db("peserta").where({ email: req.body.email });
+      const program = await req.db("program").orderBy("created_at", "asc");
+      const idpeserta = await peserta[0].id;
+      const idprogram = await program[0].id;
+      await req.db("rekap").insert({peserta: idpeserta, program: idprogram})
+      
     } else {
       req.body.updated_at = await new Date();
       await req.db("peserta").update(req.body).where({ email: req.body.email });
+
+      const peserta = await req.db("peserta").where({ email: req.body.email });
+      const program = await req.db("program").orderBy("created_at", "asc");
+      const idpeserta = await peserta[0].id;
+      const idprogram = await program[0].id;
+      await req.db("rekap").insert({peserta: idpeserta, program: idprogram})
     }
+
     const [result] = await req.db("peserta").where({ email: req.body.email });
     res.json(result);
   } catch (error) {
