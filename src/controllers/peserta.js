9882907ff21abfee1550d4b176cb5817f18e313a
fazufi <fazufi10@gmail.com>
2021-04-25 02:13:36 +0700

    nim

--- src/controllers/peserta.js
+++ src/controllers/peserta.js
@@ -32,38 +32,61 @@ exports.currentGet = async (req, res) => {
   }
 };
 
+const zeroPad = (num, places) => String(num).padStart(places, "0");
+const k = 12 + 1;
+// if (k.toString().length > 1){
+console.log(zeroPad(k, 1)); // "05"
+// }
+// ganti tahun:
+const h = new Date().getFullYear();
+console.log(h);
+if (h !== 2019) {
+  console.log("2021" + 1);
+}
+// console.log( );
+
+// console.log(zeroPad(5, 4)); // "0005"
+// console.log(zeroPad(5, 6)); // "000005"
+// console.log(zeroPad(1234, 2)); // "1234"
+
 exports.post = async (req, res) => {
   try {
-    const [isPeserta] = await req
-      .db("peserta")
-      .where({ email: req.body.email });
-      
+    const email = req.body.email;
+    const [isPeserta] = await req.db("peserta").where({ email });
+
     if (!isPeserta) {
+      const pst = await req.db("peserta").orderBy("nim", "asc");
+      const i = (await pst.length) - 1;
+      let key = "";
+      i == -1
+        ? (key = 1)
+        : (key = (await parseInt(pst[i].nim.toString().slice(4))) + 1);
+      const zeroPad = async (num, places) =>
+        await String(num).padStart(places, "0");
+
       req.body.nim =
-        (await new Date().getFullYear().toString().slice(2, 4)) +
-        Math.floor(Math.random() * 5);
+        (await new Date().getFullYear().toString()) + (await zeroPad(key, 3));
       req.body.created_at = await new Date();
-      req.body.updated_at = await 0;
+      req.body.updated_at = 0;
       await req.db("peserta").insert(req.body);
 
-      const peserta = await req.db("peserta").where({ email: req.body.email });
+      const peserta = await req.db("peserta").where({ email });
       const program = await req.db("program").orderBy("created_at", "asc");
       const idpeserta = await peserta[0].id;
       const idprogram = await program[0].id;
-      await req.db("rekap").insert({peserta: idpeserta, program: idprogram})
-      
+      await req.db("rekap").insert({ peserta: idpeserta, program: idprogram });
     } else {
       req.body.updated_at = await new Date();
-      await req.db("peserta").update(req.body).where({ email: req.body.email });
+      await req.db("peserta").update(req.body).where({ email });
 
-      const peserta = await req.db("peserta").where({ email: req.body.email });
+      const peserta = await req.db("peserta").where({ email });
       const program = await req.db("program").orderBy("created_at", "asc");
       const idpeserta = await peserta[0].id;
       const idprogram = await program[0].id;
-      await req.db("rekap").insert({peserta: idpeserta, program: idprogram})
+      await req.db("rekap").insert({ peserta: idpeserta, program: idprogram });
     }
 
-    const [result] = await req.db("peserta").where({ email: req.body.email });
+    const [result] = await req.db("peserta").where({ email });
     res.json(result);
   } catch (error) {
     res.send(error);
